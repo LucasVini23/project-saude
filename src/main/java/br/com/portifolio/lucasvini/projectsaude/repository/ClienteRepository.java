dace387733faf9ccc7e68febad2826a45c52116c
Lucas <lucasvinipro23@gmail.com>
2021-06-02 09:57:40 -0300

    Modificado a Query do Listar

--- src/main/java/br/com/portifolio/lucasvini/projectsaude/repository/ClienteRepository.java
+++ src/main/java/br/com/portifolio/lucasvini/projectsaude/repository/ClienteRepository.java
@@ -9,7 +9,7 @@ import br.com.portifolio.lucasvini.projectsaude.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-	@Query(value="SELECT * FROM cliente_lista_perfis JOIN cliente JOIN perfil on cliente_lista_perfis.cliente_id = cliente.id and cliente_lista_perfis.lista_perfis_id = perfil.id where descricao = :perfil", nativeQuery = true)
+	@Query(value="SELECT * FROM cliente_lista_Perfis JOIN cliente on cliente_lista_perfis.cliente_id = cliente.id JOIN perfil on cliente_lista_perfis.lista_perfis_id = perfil.id where descricao = :perfil", nativeQuery = true)
 	public List<Cliente> findByDescricao(String perfil);
 	
 }
